050ed7780cbd2e3ec5d6f90b094e74055e872668
Bob Clancy <rtclancy@comcast.net>
2023-05-14 08:27:18 -0400

    snapshot

--- tcl/update_all_scores.tcl
+++ tcl/update_all_scores.tcl
@@ -3,7 +3,9 @@ set user_list {
     bobs
     deans
     brians
+    wards
 }
 foreach user $user_list {
+    puts "Updating $user";
     exec ghin_calculator/ghin.tcl user_scores/$user\_scores.tcl txt > ../user_ghin/$user\_ghin.txt
 }
---
+++ tcl/user_scores/wards_scores.tcl
@@ -0,0 +1,6 @@
+set golfer_id "Williard (aka Captain Sandbagger)";
+set scores {
+    {2023/05/09 "Shennecosset"         73 }
+    {2023/04/15 "Shennecosset"         75 }
+    {2023/04/08 "Shennecosset"         77 }
+}
