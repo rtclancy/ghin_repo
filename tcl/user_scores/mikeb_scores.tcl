b72c5f8cd4a5ade4f216da731189784025167a43
U-DESKTOP-KO3V1R9\rtcla <qsirtclancy@quantum-si.com>
2023-05-25 22:16:14 -0400

    snapshot

---
+++ tcl/user_scores/mikeb_scores.tcl
@@ -0,0 +1,12 @@
+set golfer_id "Gentleman Belden";
+
+set scores {
+    {2023/05/13 "Shennecosset"          91 }
+    {2023/05/12 "Shennecosset"          88 }
+    {2023/05/11 "Shennecosset"          92 }
+    {2023/05/10 "Shennecosset"          85 }
+    {2023/05/09 "Shennecosset"          86 }
+    {2023/05/08 "Shennecosset"          86 }
+    {2023/05/07 "Shennecosset"          98 }
+    {2023/05/06 "Shennecosset"          91 }
+}
