75b72e0ff2a0d21912138ff001f11aec962e9b5c
U-DESKTOP-KO3V1R9\rtcla <qsirtclancy@quantum-si.com>
2023-07-02 19:23:18 -0400

    snapshot

--- tcl/user_scores/deans_scores.tcl
+++ tcl/user_scores/deans_scores.tcl
@@ -1,5 +1,6 @@
 set golfer_id "Deano Tomato Mozzarella";
 set scores {
+    {2023/07/23 "Hunter White"              106 }
     {2023/04/16 "Lyman Jones White"         104 }
     {2023/04/15 "Shennecosset"              112 }
     {2023/04/08 "Shennecosset"              102 }
